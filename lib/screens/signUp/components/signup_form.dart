939f658060f6a3de2ae44b9e3d6847fcb23f2dae
Hugo Maia <hugomaia75@gmail.com>
2023-03-17 13:53:56 -0300

    Signup complete

--- lib/screens/signUp/components/signup_form.dart
+++ lib/screens/signUp/components/signup_form.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
-
 import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   const SignUpForm({
     Key? key,
   }) : super(key: key);
 
   @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  String _gender = '';
+
+  @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const SizedBox(height: defaultPadding),
+          const SizedBox(height: defaultPadding),
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
@@ -42,10 +51,102 @@ class SignUpForm extends StatelessWidget {
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "Confirm password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
           const SizedBox(height: defaultPadding / 2),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Sign Up".toUpperCase()),
+          const Center(
+            child: Text(
+              'Gender',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: [
+              Radio<String>(
+                value: 'Male',
+                groupValue: _gender,
+                onChanged: (value) {
+                  setState(() {
+                    _gender = value!;
+                  });
+                },
+              ),
+              const Text('Male'),
+            ],
+          ),
+          Row(
+            children: [
+              Radio<String>(
+                value: 'Female',
+                groupValue: _gender,
+                onChanged: (value) {
+                  setState(() {
+                    _gender = value!;
+                  });
+                },
+              ),
+              const Text('Female'),
+            ],
+          ),
+          Row(
+            children: [
+              Radio<String>(
+                value: 'Other',
+                groupValue: _gender,
+                onChanged: (value) {
+                  setState(() {
+                    _gender = value!;
+                  });
+                },
+              ),
+              const Text('Other'),
+            ],
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFormField(
+            keyboardType: TextInputType.datetime,
+            cursorColor: kPrimaryColor,
+            onSaved: (dateOfBirth) {},
+            decoration: const InputDecoration(
+              hintText: "Date of Birth",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.calendar_today),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFormField(
+            keyboardType: TextInputType.phone,
+            cursorColor: kPrimaryColor,
+            onSaved: (phone) {},
+            decoration: const InputDecoration(
+              hintText: "Phone number",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.phone),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text("Sign Up".toUpperCase()),
+            ),
           ),
           const SizedBox(height: defaultPadding),
           AlreadyHaveAnAccountCheck(
