16a4704bd177e1a0990e4212674df319d381b290
Hugo Maia <hugomaia75@gmail.com>
2023-05-09 20:22:37 -0300

    Firebase OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_page/constants.dart';
 import 'package:flutter_login_page/screens/welcome/welcome_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
